d1c3ec7648d7b43f6c15e55d4582a5ad03b10f73
thomas <info@englishreservoir.com>
2024-09-17 18:37:28 +0200

    hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,40 +2,24 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-between h-screen p-8 bg-gray-100">
-      {/* Left Section: H1 */}
-      <div className="lg:w-1/2 w-full flex items-center justify-center text-center lg:text-left">
-        <h1 className="text-4xl lg:text-6xl font-bold text-gray-900">
-          Welcome to Our Platform!
-        </h1>
-      </div>
-      {/* Right Section: Image */}
-      <div className="lg:w-1/2 w-full flex items-center justify-center">
-        <img
-          className="w-80 h-80 object-cover"
-          src="src/assets/20.svg"
-          alt="Hero Image"
-        />
-      </div>
-    </div>
-  );
-}
-export default Hero;
-
-
-{/*const Hero = ({
-  title = "This is a title",
-  subtitle = "This is a subtitle",
-}) => {
-  return (
-    <section className="bg-indigo-700 py-20 mb-4">
-      <div className="max-w-7xl.mx-auto.px-4 flex flex-col items-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold text-white">{title}</h1>
-          <p className="my-4 text-xl text-white">{subtitle}</p>
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="relative isolate overflow-hidden px-6 pt-16 sm:px-16 md:pt-24 lg:flex lg:gap-x-32 lg:px-24 lg:pt-0">
+          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">
+            Learn English in a Fun, Simple and Dynamic Way!
+            </h2>
+          </div>
+          <div className="mx-auto relative mt-16 h-80 lg:mt-8">
+            <img
+              alt="english courses"
+              src="/src/assets/category-2.svg" className="mx-auto"
+            />
+          </div>
         </div>
       </div>
-    </section>
-  );
-};
-export default Hero; */}
+    </div>
+  )
+}
+
+export default Hero;
