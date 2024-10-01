bb2abf37d12257cae0acd4b132aa0e7d15018079
thomas <info@englishreservoir.com>
2024-10-01 17:04:26 +0200

    new styling for Curriculum.jsx

--- src/pages/courses/A1/Curriculum.jsx
+++ src/pages/courses/A1/Curriculum.jsx
@@ -3,27 +3,25 @@ import { FaCheckCircle } from "react-icons/fa"; // Importing an icon from react-
 
 const Curriculum = () => {
   return (
-    <section class="py-8 md:py-16 dark:bg-gray-900">
+    <section class="py-8 md:py-16 bg-greenAone">
       <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
-        <h2 class="text-3xl md:text-4xl text-center font-semibold font-dosis text-white py-4">
+        <h2 class="text-4xl md:text-6xl text-center font-semibold font-dosis text-white py-4">
           Study Plan
         </h2>
 
-        <div className="grid place-items-center py-8 px-8 bg-mellowGreen">
-          <div className="grid grid-cols-[20%_80%] w-9/12 bg-black rounded-3xl p-8 flex justify-items-start">
-            <div className="flex justify-center items-center bg-white">
-              <FaCheckCircle className="text-blue-500 text-5xl" />
+        <div className="grid place-items-center py-8 px-8">
+          <div className="flex items-center justify-center w-10/12 bg-white rounded-3xl p-8">
+            <div className="hidden md:flex justify-center items-center bg-white rounded-full p-4 mr-6">
+              <FaCheckCircle className="text-blue-500 text-6xl md:text-7xl lg:text-8xl" />
             </div>
 
-            <div className="flex justify-items-start text-left bg-white">
-              <div className="bg-mellowGreen">
-                <h1>UNIT 1</h1>
-                <ul className="list-disc list-inside text-white">
-                  <li>Item 1</li>
-                  <li>Item 2</li>
-                  <li>Item 3</li>
+            <div className="bg-[#5271ff] p-4 rounded-xl w-full text-white">
+                <h1 className="text-2xl font-bold mb-4 text-center">UNIT 1</h1>
+                <ul className="list-disc pl-5 space-y-2 text-lg font-semibold">
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+                  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
                 </ul>
-              </div>
             </div>
           </div>
         </div>
