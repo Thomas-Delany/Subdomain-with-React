366503ea309abd275fa6e810d20bcf2674d2c88d
thomas <info@englishreservoir.com>
2025-01-13 18:20:26 +0100

    centralising course product array

--- src/pages/courses/A1/BreakThrough.jsx
+++ src/pages/courses/A1/BreakThrough.jsx
@@ -3,13 +3,24 @@ import Hero from "./Hero";
 import Curriculum from "./Curriculum";
 import NextLevel from "./NextLevel";
 import CoursesDisplay from "./CoursesDisplay";
+import { courseData } from "../../../data/courseData";  
 
 const BreakThrough = () => {
+  const currentCourseIndex = courseData.findIndex(
+    (product) => product.name === "BreakThrough"
+  );
+
+  const nextCourse = courseData[currentCourseIndex + 1] || null;
+
   return (
     <div>
     <Hero />
     <Curriculum />
-    <NextLevel category="A2" name="WayStage" />
+    {nextCourse ? (
+        <NextLevel category={nextCourse.category} name={nextCourse.name} />
+      ) : (
+        <h2>No Next Level</h2>
+      )}
     <CoursesDisplay />
     </div>
   );
